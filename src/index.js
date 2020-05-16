cda379a15d8be447b3e7397c2dc1880909863c49
Rodolfo Goncalves <rodolfo.goncalves@blip.pt>
2020-05-16 20:07:41 +0100

    add translations mechanism

--- src/index.js
+++ src/index.js
@@ -1,10 +1,20 @@
 import menu from "./data/menu";
 import translations from "./data/translations";
-
-import webcomponents from "./components"
+import webcomponents from "./components";
+let selectedLang = navigator.language === "pt" ? "pt" : "en";
 
 customElements.define("alfranco-header", webcomponents.header);
 customElements.define("alfranco-menu", webcomponents.menu);
 customElements.define("alfranco-footer", webcomponents.footer);
 
-document.getElementById("root").innerHTML = `<alfranco-menu data-menu='${JSON.stringify(menu.pt)}'></alfranco-menu>`
+function render(lang = "pt") {
+  document.getElementById("swith-lang-btn").innerText = lang.toUpperCase();
+  document.getElementById("root").innerHTML = `<alfranco-menu data-menu='${JSON.stringify(menu[lang])}'></alfranco-menu>`
+};
+
+document.getElementById("swith-lang-btn").addEventListener("click", function eventHandler(e) {
+  selectedLang = selectedLang === "pt" ? "en" : "pt";
+  render(selectedLang);
+});
+
+render(selectedLang);
